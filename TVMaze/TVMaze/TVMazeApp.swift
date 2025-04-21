f6fd117ae5c5487b05d811ec671b282bed072049
Juan David Lopera Lopez <juandavidl2011.jdll@gmail.com>
2025-04-21 16:17:28 -0500

    Creating project
    Creating project in TVMaze repo

---
+++ TVMaze/TVMaze/TVMazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVMazeApp.swift
+//  TVMaze
+//
+//  Created by Juan david Lopera lopez on 21/04/25.
+//
+
+import SwiftUI
+
+@main
+struct TVMazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TVMaze/TVMazeTests/TVMazeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TVMazeTests.swift
+//  TVMazeTests
+//
+//  Created by Juan david Lopera lopez on 21/04/25.
+//
+
+import Testing
+@testable import TVMaze
+
+struct TVMazeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
