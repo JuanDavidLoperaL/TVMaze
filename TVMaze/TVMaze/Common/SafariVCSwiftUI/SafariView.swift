87449a0862acb750a446bc58343d07d5a4c9bd8e
Juan David Lopera Lopez <juandavidl2011.jdll@gmail.com>
2025-04-26 16:32:35 -0500

    Load episode detail of each character
    Load episode detail of each character

---
+++ TVMaze/TVMaze/Common/SafariVCSwiftUI/SafariView.swift
@@ -0,0 +1,20 @@
+//
+//  SafariView.swift
+//  TVMaze
+//
+//  Created by Juan david Lopera lopez on 26/04/25.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    let url: URL
+
+    func makeUIViewController(context: Context) -> SFSafariViewController {
+        return SFSafariViewController(url: url)
+    }
+
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: Context) {
+    }
+}
